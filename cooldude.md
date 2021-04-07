c3a0cb553513a171ac6b5d1cf1065ec1534a35fb
ksdevinney <katie@devinney.org>
2021-04-06 20:38:01 -0500

    add more questions, test created readme

---
+++ cooldude.md
@@ -0,0 +1,39 @@
+
+        # cool dude
+        
+        ## Description
+        
+        hella cool, bro
+
+        ## Table of Contents 
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [License](#license)
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [Questions](#questions)
+        
+        ## Installation
+
+        spread it on toast
+
+        ## Usage
+
+        you
+
+        ## License
+
+        ## Contributing
+
+        sure
+
+        ## Tests
+
+        no thanks, I don't want it to break
+
+        ## Questions
+
+        Please contact me at katie@katie.katie if you have any questions.
+
+        [My GitHub profile can be found here.](https://github.com/katie)
+        
--- index.js
+++ index.js
@@ -12,19 +12,88 @@ inquirer
             type: 'input',
             message: 'Write a description of your project.',
             name: 'description',
-        }
+        },
+        {
+            type: 'input',
+            message: 'How do you install it?',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'Write a description of your project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'How do you use it?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Contributions?????',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: 'Tests??????',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'username',
+        },
     ])
 
     .then((response) => {
         const fileName = `${response.title.toLowerCase().split(' ').join('')}.md`;    
 
         const newReadme = `
-        # ${response.name}
+        # ${response.title}
+        
         ## Description
+        
         ${response.description}
+
+        ## Table of Contents 
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [License](#license)
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [Questions](#questions)
+        
+        ## Installation
+
+        ${response.install}
+
+        ## Usage
+
+        ${response.usage}
+
+        ## License
+
+        ## Contributing
+
+        ${response.contributing}
+
+        ## Tests
+
+        ${response.test}
+
+        ## Questions
+
+        Please contact me at ${response.email} if you have any questions.
+
+        [My GitHub profile can be found here.](https://github.com/${response.username})
         `;
         
         fs.writeFile(fileName, newReadme, (err) =>
-            err ? console.log (err) : console.log('Success!')
+            err ? console.log (err) : console.log('Success! Look in the folder!')
             );
     });
